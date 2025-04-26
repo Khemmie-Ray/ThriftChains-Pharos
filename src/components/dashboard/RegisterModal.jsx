2639ef4af6796aa975b118ea78663da475dd1dee
Khemmie-Ray <atokemmy@gmail.com>
2025-04-26 13:02:09 +0100

    integrations

--- src/components/dashboard/RegisterModal.jsx
+++ src/components/dashboard/RegisterModal.jsx
@@ -1,8 +1,20 @@
 import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
 import { useState } from 'react'
+import useRegister from '../../hooks/useRegister'
 
  const RegisterModal = () => {
   let [isOpen, setIsOpen] = useState(true)
+  const [username, setUsername] = useState("")
+  const [isLister, setIslister] = useState(false)
+  const [error, setError] = useState("");
+  const handleRegister = useRegister();
+
+  const handleSignup = async () => {
+    await handleRegister(username, isLister);
+   setIslister(null)
+   setUsername("")
+  };
+
 
   function open() {
     setIsOpen(true)
@@ -22,25 +34,29 @@ import { useState } from 'react'
       </Button>
 
       <Dialog open={isOpen} as="div" className="relative z-10 focus:outline-none" onClose={close}>
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto bg-black/70">
           <div className="flex min-h-full items-center justify-center p-4">
             <DialogPanel
               transition
-              className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+              className="w-full max-w-md rounded-xl bg-black/80 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] text-white "
             >
-              <DialogTitle as="h3" className="text-base/7 font-medium text-white">
-                Payment successful
+              <DialogTitle as="h3" className="text-base/7 font-medium text-white text-[24px] text-center my-6">
+                Register as a Thrifter
               </DialogTitle>
-              <p className="mt-2 text-sm/6 text-white/50">
-                Your payment has been successfully submitted. We’ve sent you an email with all of the details of your
-                order.
-              </p>
+             <p className='mb-2'>Username</p>
+             <input type="text" placeholder='Enter username' className='border mb-4 border-white/20 w-[100%] rounded-md hover:outline-0 p-3' onChange={(e) => setUsername(e.target.value)} value={username}/>
+             <p className='mb-2'>Are you an Asset Lister?</p>
+             <select name="" id="" className='border border-white/20 w-[100%] rounded-md hover:outline-0 p-3 mb-4' onChange={(e) => setIslister(e.target.value)} value={isLister}>
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+
+             </select>
               <div className="mt-4">
                 <Button
-                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
-                  onClick={close}
+                  className="bg-linear-to-r from-primary to-lilac py-4 px-8 rounded-full font-[500] w-[100%]"
+                  onClick={handleSignup}
                 >
-                  Got it, thanks!
+                 Register
                 </Button>
               </div>
             </DialogPanel>
--- src/hooks/useSignerOrProvider.js
+++ src/hooks/useSignerOrProvider.js
@@ -1,7 +1,7 @@
 import { useAppKitProvider } from "@reown/appkit/react";
 import { BrowserProvider } from "ethers";
 import { useEffect, useMemo, useState } from "react";
-import { readOnlyProvider } from "../constants/readOnlyProvideer";
+import { readOnlyProvider } from "../constants/readOnlyProvider";
 import { ethers } from "ethers";
 
 const useSignerOrProvider = () => {
